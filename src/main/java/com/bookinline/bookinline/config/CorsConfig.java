59072410221136cc88ac058b5c72569146ccae09
Vladyslav Kokitko <koks.vladislav277@gmail.com>
2025-07-10 18:25:16 +0200

    cors for another fe domain

--- src/main/java/com/bookinline/bookinline/config/CorsConfig.java
+++ src/main/java/com/bookinline/bookinline/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -22,9 +23,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(isProd ? "https://bookinline.vercel.app" : "http://localhost:5173")
+                        .allowedOrigins(isProd ? "https://bookinline.onrender.com" : "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
+                        .allowedHeaders("Content-Type", "X-XSRF-TOKEN", "Authorization", "refreshToken")
                         .allowCredentials(true);
             }
         };
